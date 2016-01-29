9f1ee0dc8e5215fce90cb8819b441e3338e7e93f
Viktor Palmkvist <viktor@palmkvist.com>
2016-01-29 17:02:10 +0100

    Fixed project structure, removing old versions

--- figwheel/env/prod/cljs/scribe/prod.cljs
+++
@@ -1,7 +0,0 @@
-(ns scribe.prod
-  (:require [scribe.core :as core]))
-
-;;ignore println statements in prod
-(set! *print-fn* (fn [& _]))
-
-(core/init!)
--- standalone-client/scripts/watch.clj
+++
@@ -1,6 +0,0 @@
-(require '[cljs.build.api :as b])
-
-(b/watch "src"
-  {:main 'standalone-client.core
-   :output-to "out/standalone_client.js"
-   :output-dir "out"})
